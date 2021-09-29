113b0c89c2cb3edd6e2bbf3cfdfdd00ca3e8d0b5
Ademie <isijolaademie@gmail.com>
2021-09-29 09:15:42 +0100

    responsive

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
+    current[0].className = current[0].className.replace(" active", " ");
     this.className += " active";
   });
 }
